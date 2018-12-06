9fe68afab17de121d88f14127e93b8aaf0943aea
ville <lilinitsy@gmail.com>
2018-12-06 10:54:33 -0600

    Push Unity

---
+++ Assets/Scripts/Lidar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lidar : MonoBehaviour
+{
+	public float degree_sweep_increment = 1.0f;
+	public float max_height_angle = 30.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
